1c2cc8ad63cd0db7068a9b7f70d98391a6410bcc
anosvoldigo23-collab <anosvoldigo23@gmail.com>
2025-10-18 15:53:40 +0000

    Update botsay.js

--- scripts/cmds/botsay.js
+++ scripts/cmds/botsay.js
@@ -1,22 +1,26 @@
-module.exports = {
-	config: {
-		name: "botsay",
-		version: "1.0",
-		author: "Christus",
-		role: 0,
-		category: "texte",
-		guide: {
-			fr: "botsay + (Message que vous voulez que le bot répète)"
-		}   
-	},
+const g = require("fca-aryan-nix"); // GoatWrapper pour noprefix
 
-	onStart: async function ({ api, args, event }) {
-		const say = args.join(" ");
-		if (!say) {
-			api.sendMessage("Veuillez entrer un message", event.threadID, event.messageID);
-		} else {
-			api.sendMessage(`${say}`, event.threadID, event.messageID);
-		}
-	}
-
+module.exports.config = {
+  name: "botsay",
+  version: "1.0",
+  author: "Christus",
+  role: 0,
+  category: "texte",
+  guide: {
+    fr: "botsay + (Message que vous voulez que le bot répète)"
+  },
+  noPrefix: true // Activation noprefix
 };
+
+module.exports.onStart = async function({ api, args, event }) {
+  const say = args.join(" ");
+  if (!say) {
+    api.sendMessage("Veuillez entrer un message", event.threadID, event.messageID);
+  } else {
+    api.sendMessage(say, event.threadID, event.messageID);
+  }
+};
+
+// Active le mode noprefix via GoatWrapper
+const w = new g.GoatWrapper(module.exports);
+w.applyNoPrefix({ allowPrefix: false });
